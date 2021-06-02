0c921626446cc03e39f4a7f7b780157d35790c89
Esther Ukandu <esthere.amara@gmail.com>
2021-06-02 21:06:30 +0100

    land banking investment page

--- resources/views/landBankingInvestment.blade.php
+++ resources/views/landBankingInvestment.blade.php
@@ -4,4 +4,39 @@
     <section class="bg-center bg-cover bg-no-repeat flex h-96 items-center justify-center pt-12" style="background-image: url({{ asset('/assets/landbank.jpg') }})">
         <p class="font-semibold home text-white bebas-neue"> Land Banking Investment </p>
     </section>
+
+    <section class="mx-auto my-24 w-2/3">
+        <p class="font-bold mb-5 text-3xl text-gray-700"> Heard of Land Banking Investment?</p>
+        <p class="leading-6 text-lg">Simply means investing your money into real estate. Now with Landvest by Tribitat you can invest as low as 100k (one hundred thousand Naira) and earn up to 60-72% returns on investments yearly.</p>
+    </section>
+
+    <hr />
+
+    <section class="mx-auto my-20 w-2/3">
+        <p class="font-bold mb-5 text-3xl text-gray-700"> Why Landvest? </p>
+
+        <div class="leading-6 text-lg">
+            <p>Landvest is a flexible real estate investment initiative that creates the opportunity for everyone to earn within 15% to 72% returns on real estate investments over a period of 6 months to 24 months from as low as N100,000 for a unit of property per square meter.</p>
+
+            <p class="mt-5">Landvest Property investment initiative is a viable options for those who want to invest in real estate but do not want to buy a physical landed property and have to deal with the hassle of buying and selling them later.</p>
+
+            <p class="font-semibold italic mt-5 text-gray-500">Interesting right?</p>
+
+            <p class="mt-5">A lot of investment options ranging from bonds to stock, fixed deposits and real estate are available in nations around the world but the billionaires chose to learn how to invest their funds to make more returns.</p>
+
+            <p class="mt-5">For those who know how to play the game of numbers, the Nigerian real estate industry is a gold mine. In just five years, rapid development has overtaken the industry, and major real estate companies are creating a shift in the – “I cannot own a Property mindset.” </p>
+
+            <p class="mt-5">Leveraging on these changes, Tribitat Real Estate Limited, one of the fastest-growing real companies in Nigeria, has created its new investment product – Landvest, for everyone that would love to invest their hard earned money into real estate with ease.</p>
+
+            <p class="font-semibold italic mt-5 text-gray-500">Now hear this...</p>
+
+            <p class="mt-5">Landvest is the best form of real estate investments initiative for everyone who loves their sleep and peace of mind because landvest offer you:</p>
+
+            <ul class="list-disc ml-14 mt-2">
+                <li>Security of investment</li>
+                <li>Position you ahead of instability and frequent devaluation of the Naira</li>
+                <li>Physical assets to back up investments</li>
+            </ul>
+        </div>
+    </section>
 @endsection
