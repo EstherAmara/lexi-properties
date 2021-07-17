ca88afac7cafca35656e12484071d375707eab58
Esther Ukandu <esthere.amara@gmail.com>
2021-07-17 22:56:31 +0100

    carousel for property images

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -123,6 +123,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js" integrity="sha512-XtmMtDEcNz2j7ekrtHvOVR4iwwaD6o/FUJe6+Zq+HgcCsk3kj4uSQQR8weQ2QVj1o0Pk6PwYLohm206ZzNfubg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
     <script>
+        $('.your-class').slick();
         document.getElementById('showNav').addEventListener('click', function(e) { document.getElementById('mobileNav').classList.toggle('hidden')})
     </script>
 </body>
